2983ed399419615116ecc78cb20ad0dcb18271d0
Carissa Ward <cward52@my.bcit.ca>
2024-01-08 15:36:37 -0800

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using IfRolesExample.Models;
+
+namespace IfRolesExample.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<MyRegisteredUser> MyRegisteredUsers { get; set; }
+    }
+}
