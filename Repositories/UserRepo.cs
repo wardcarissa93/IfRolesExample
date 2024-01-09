2020b60ed1c9ce2d0367f14ed58195946360dd71
Carissa Ward <cward52@my.bcit.ca>
2024-01-09 11:49:49 -0800

    UserRepo added

---
+++ Repositories/UserRepo.cs
@@ -0,0 +1,25 @@
+﻿using IfRolesExample.Data;
+using IfRolesExample.ViewModels;
+
+namespace IfRolesExample.Repositories
+{
+    public class UserRepo
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserRepo(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+
+        public List<UserVM> GetAllUsers()
+        {
+            var userEmails = _context.Users.Select(u => new UserVM
+            {
+                Email = u.Email
+            }).ToList();
+
+            return userEmails;
+        }
+    }
+}
---
+++ ViewModels/UserVM.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IfRolesExample.ViewModels
+{
+    public class UserVM
+    {
+        [Key]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
+}
