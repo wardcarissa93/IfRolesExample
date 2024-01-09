9fa433484dbf9cea23d918abbb1aad95163950d8
Carissa Ward <cward52@my.bcit.ca>
2024-01-08 16:13:31 -0800

    Create Role added

--- Controllers/RoleController.cs
+++ Controllers/RoleController.cs
@@ -22,5 +22,32 @@ namespace IfRolesExample.Controllers
 
             return View(roles);
         }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(RoleVM roleVM)
+        {
+            if (ModelState.IsValid)
+            {
+                RoleRepo roleRepo = new RoleRepo(_db);
+                bool isSuccess = roleRepo.CreateRole(roleVM.RoleName);
+
+                if (isSuccess)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Role creation failed." +
+                                             " The role may already exist.");
+                }
+            }
+            return View(roleVM);
+        }
+
     }
 }
