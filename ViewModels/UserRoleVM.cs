c5c1593a6aafaa5b56238be6fab874a9ca1c0c4b
Carissa Ward <cward52@my.bcit.ca>
2024-01-09 12:55:58 -0800

    Added UserRoleVM, repo, and controller

---
+++ ViewModels/UserRoleVM.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IfRolesExample.ViewModels
+{
+    public class UserRoleVM
+    {
+        [Display(Name = "ID")]
+        public string? Id { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Role")]
+        public string Role { get; set; }
+    }
+}
